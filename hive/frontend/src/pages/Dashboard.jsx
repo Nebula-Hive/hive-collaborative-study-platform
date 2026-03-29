f254d96338a64ec86aa7cc733001491c9139836f
Uvini Ranasinghe <uviniranasinghe21@gmail.com>
2026-03-29 21:07:08 +0530

    remove session list view from the dashboard for the students

--- hive/frontend/src/pages/Dashboard.jsx
+++ hive/frontend/src/pages/Dashboard.jsx
@@ -46,7 +46,7 @@ export default function Dashboard() {
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         {/* Left: Calendar */}
         <div className="lg:col-span-2 space-y-6">
-          <StudySessionCalendar isUpcomingTasks={false} />
+          <StudySessionCalendar isUpcomingTasks={false} hideListView={true} />
         </div>
 
         {/* Right: Sidebar widgets */}
